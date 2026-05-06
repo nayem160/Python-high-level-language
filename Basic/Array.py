c7ac418c728060781c1ee3ed382a096b790cf19d
Arafat Nayem <yanayem01@gmail.com>
2026-05-07 02:02:36 +0600

    Create Array.py

---
+++ Basic/Array.py
@@ -0,0 +1,7 @@
+car = ["Volvo", "X-car", "Volkswagen", "Toyota", "Mazda"]
+car.append("Range Rover")
+car.remove("Volvo")
+lenth = len(car)
+print("Number of car", lenth)
+for i in range(lenth):
+    print(car[i])
